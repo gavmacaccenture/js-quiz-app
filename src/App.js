f1b54e001db43ee1fc3e581cb9a860c32c909a62
Gav McClary <gavin.mcclary@accenture.com>
2022-11-01 15:55:06 +0000

    Blurgh

--- src/App.js
+++ src/App.js
@@ -1,25 +1,90 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Functions from "./images/Functions.png";
 
-function App() {
+export default function App() {
+  const questions = [
+    {
+      questionText: "What does this code do?",
+      answerOptions: [
+        { answerText: "Log Hello World when called", isCorrect: false },
+        {
+          answerText: "Defines a function called myFunction()",
+          isCorrect: false,
+        },
+        { answerText: "Is valid JavaScript", isCorrect: false },
+        { answerText: "All of the above", isCorrect: true },
+      ],
+    },
+    {
+      questionText: "Some question about JavaScript: Answer is B",
+      answerOptions: [
+        { answerText: "A", isCorrect: false },
+        { answerText: "B", isCorrect: true },
+        { answerText: "C", isCorrect: false },
+        { answerText: "D", isCorrect: false },
+      ],
+    },
+    {
+      questionText: "Some question about JavaScript: Answer is C",
+      answerOptions: [
+        { answerText: "A", isCorrect: false },
+        { answerText: "B", isCorrect: false },
+        { answerText: "C", isCorrect: true },
+        { answerText: "D", isCorrect: false },
+      ],
+    },
+    {
+      questionText: "Some question about JavaScript: Answer is D",
+      answerOptions: [
+        { answerText: "A", isCorrect: false },
+        { answerText: "B", isCorrect: false },
+        { answerText: "C", isCorrect: false },
+        { answerText: "D", isCorrect: true },
+      ],
+    },
+  ];
+
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [showScore, setShowScore] = useState(false);
+  const [score, setScore] = useState(0);
+
+  const handleAnswerOptionClick = (isCorrect) => {
+    if (isCorrect) {
+      setScore(score + 1);
+    }
+
+    const nextQuestion = currentQuestion + 1;
+    if (nextQuestion < questions.length) {
+      setCurrentQuestion(nextQuestion);
+    } else {
+      setShowScore(true);
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      {showScore ? (
+        <div className="score-section">
+          You scored {score} out of {questions.length}
+        </div>
+      ) : (
+        <>
+          <div>
+            <img src={Functions} alt="Functions"></img>
+          </div>
+          <div className="question-text">
+            {questions[currentQuestion].questionText}
+          </div>
+          <div className="answer-section">
+            {questions[currentQuestion].answerOptions.map((answerOption) => (
+              <button
+                onClick={() => handleAnswerOptionClick(answerOption.isCorrect)}
+              >
+                {answerOption.answerText}
+              </button>
+            ))}
+          </div>
+        </>
+      )}
     </div>
   );
 }
-
-export default App;
